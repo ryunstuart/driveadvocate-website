748f602bca58fad96d249e4f8bd52cdd5a727bc9
Ryun Stuart <info@stubros.com>
2026-06-22 21:26:12 -0500

    Fix duplicate nextConfig

--- next.config.js
+++ next.config.js
@@ -1,27 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
-
-module.exports = nextConfig;
-const nextConfig = {
-  output: 'export',
   distDir: 'out',
   trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/dashboard.html',
-        destination: '/dashboard.html',
-      },
-    ];
   },
 };
 
