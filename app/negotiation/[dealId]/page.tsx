700a26f07b694aa999e110c0ba18e76a05271ef4
Ryun Stuart <info@stubros.com>
2026-06-24 03:23:57 -0500

    Add advocate intake form at /advocate/intake

--- app/negotiation/[dealId]/page.tsx
+++ app/negotiation/[dealId]/page.tsx
@@ -1,7 +1,13 @@
 import ClientDealFile from './ClientDealFile';
 
 export async function generateStaticParams() {
-  return [{ dealId: 'placeholder' }];
+  return [
+    { dealId: 'placeholder' },
+    { dealId: 'deal-001' },
+    { dealId: 'deal-002' },
+    { dealId: 'deal-003' },
+    { dealId: 'deal-004' },
+  ];
 }
 
 export default function DealFilePage() {
